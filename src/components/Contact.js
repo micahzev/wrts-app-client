0488912a14b0dcd5eaf5b27d4045dae88ae7085a
micahzev <friedland.micah@gmail.com>
2017-08-25 16:54:06 +0200

    contacts overlay updated

---
+++ src/components/Contact.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import '../styles/contact.css';
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.hideMe.bind(this);
+    this.state ={
+      show:false,
+    };
+  }
+
+
+  componentDidUpdate() {
+      this.setState({
+        show:this.props.show,
+      });
+
+  }
+
+  hideMe() {
+    this.setState({
+      show:false,
+    });
+    this.props.undoShow();
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.show?
+          <div className="contactOverlay">
+            <div className="closeButton" onClick={this.hideMe.bind(this)}>
+            </div>
+
+              <div className="parentText">
+                  <div className="contactTextUL">
+                    WE
+
+                    RUN
+                  </div>
+                  <div className="contactTextUR">
+                    THE
+                    
+                     SPACE
+                  </div>
+
+              </div>
+
+             <div className="contactText">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+              volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequa
+              t. Duis autem vel eum iriure dolor in hendrerit in vulputate velit
+               esse molestie consequat, vel illum dolore eu feugiat nulla facilisi
+               s at vero eros et accumsan et iusto odio dignissim qui blandit prae
+               sent luptatum zzril delenit augue duis dolore te feugait nulla facil
+               isi. Nam liber tempor cum soluta nobis eleifend option congue nihil
+                imperdiet doming id quod mazim placerat facer possim assum.
+             </div>
+           </div>
+          : null}
+
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/components/Spaces.js
+++ src/components/Spaces.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Space from './Space';
+import Contact from './Contact'
 
 import '../styles/spaces.css';
 
@@ -9,6 +10,22 @@ class Spaces extends Component {
 
   constructor(props) {
     super(props);
+    this.contactsOverlay.bind(this);
+    this.state = {
+      showOverLay: false,
+    }
+  }
+
+  contactsOverlay() {
+      this.setState({
+        showOverLay:true,
+      })
+  }
+
+  undoShow(){
+    this.setState({
+      showOverLay:false,
+    })
   }
 
   render() {
@@ -17,6 +34,8 @@ class Spaces extends Component {
 
     return (
       <div>
+        <p className="contactButton" onClick={this.contactsOverlay.bind(this)}>contact</p>
+          <Contact show={this.state.showOverLay} undoShow={this.undoShow.bind(this)}/>
         <div className="Space">
             {allSpaces.map((spaceData,idx) =>
               <Space key={idx} space={spaceData} />

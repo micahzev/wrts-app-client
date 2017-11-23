a624b77b16e8c910777c621105a68b6875c8b101
micahzev <friedland.micah@gmail.com>
2017-11-23 16:20:06 +0200

    migration

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ module.exports = {
   apiHost: window.APIHOST || 'localhost',
   apiPort: window.APIPORT || 1337,
   apiProtocol: window.APIPROTOCOL || 'http',
-  apiURL: 'https://6g9e4r6rig.execute-api.us-east-1.amazonaws.com/prod/',
+  apiURL: 'https://ceodpwyxgc.execute-api.us-east-1.amazonaws.com/prod/',
   cognito: {
-    USER_POOL_ID : 'us-east-1_VUHBoTEE9',
-    APP_CLIENT_ID : '36m80n54g5enrick8mdda6sk5b',
+    USER_POOL_ID : 'us-east-1_yNPBC3rUw',
+    APP_CLIENT_ID : '775jtbu6knkv0k5mfd3i5jdsru',
   }
 };
